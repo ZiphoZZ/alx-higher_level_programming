0e9f806180fdfc74acacac13e9ba35e4da4191d8
ZiphoZZ <nkunkuzazz@gmail.com>
2024-03-11 12:18:22 -0700

    Second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
-const process = require('process');
-const argv = process.argv;
-const argc = process.argv.length;
-if (argc < 4) {
-  console.log(0);
-} else {
-  const array = argv.slice(2).sort((a, b) => a - b).reverse();
-  console.log(array[1]);
-}
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
 
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
