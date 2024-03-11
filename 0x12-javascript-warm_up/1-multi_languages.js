7d1eeb416062df0c650f7ed785f4c5fd57afac95
ZiphoZZ <nkunkuzazz@gmail.com>
2024-03-11 12:02:47 -0700

    Languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+/* The 1st line needs to be "C is fun"
+ * The 2nd line needs to be "Python is cool"
+ * The 3rd line needs to be "JavaScript is amazing"
+ */
+const FirstLine = 'C is fun';
+const SecondLine = 'Python is cool';
+const ThirdLine = 'JavaScript is amazing';
+console.log(FirstLine);
+console.log(SecondLine);
+console.log(ThirdLine);
