e9c86608b58bd3f4143208155090124086ec9a61
ZiphoZZ <nkunkuzazz@gmail.com>
2024-03-11 12:28:07 -0700

    Fourth push

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-function add (a, b) {
-  console.log(parseInt(a) + parseInt(b));
-}
-add(process.argv[2], process.argv[3]);
+const a = process.argv[2];
+const b = process.argv[3];
 
+function add (a, b) {
+  return (a + b);
+}
+
+console.log(add(parseInt(a), parseInt(b)));
