38278afa1671a800bebfecb98bc97fe53848af95
ZiphoZZ <nkunkuzazz@gmail.com>
2024-03-12 10:07:48 -0700

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (!w || !h) {
+      return;
+    }
+    if (h > 0 && w > 0) {
       this.width = w;
       this.height = h;
     }
