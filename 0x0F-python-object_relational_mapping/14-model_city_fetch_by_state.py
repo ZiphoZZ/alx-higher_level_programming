348878d4165b57c65f637aba09c8e5411b02b215
ZiphoZZ <113602509+ZiphoZZ@users.noreply.github.com>
2024-03-21 19:20:44 +0200

    Update 14-model_city_fetch_by_state.py
    changes

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,24 +1,25 @@
-
 #!/usr/bin/python3
-"""prints all the cities"""
+"""
+Script that prints all City objects from the database
+"""
+from model_city import City
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-if __name__ == '__main__':
-    from sys import argv
-    from model_state import Base, State
-    from model_city import City
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-
-    USER = argv[1]
-    PASS = argv[2]
-    DB = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(USER, PASS, DB), pool_pre_ping=True)
-
+if __name__ == "__main__":
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
     Base.metadata.create_all(engine)
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    for cities in session.query(City):
-        for states in session.query(State).filter(State.id == cities.state_id):
-            print("{}: ({}) {}".format(states.name, cities.id, cities.name))
+    city = session.query(State, City).join(City).order_by(City.id)
+    for state, city in city:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    # Close session
+    session.close()
