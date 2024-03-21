07b8f22c0540ea6fb5d2e5b37e864c69c97086f4
ZiphoZZ <113602509+ZiphoZZ@users.noreply.github.com>
2024-03-21 19:14:40 +0200

    Update 11-model_state_insert.py
    changes

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,24 +1,27 @@
-
 #!/usr/bin/python3
-"""A script that lists all states"""
+"""
+Script that adds the State object “Louisiana” to the database
+Using module SQLAlchemy
+"""
 
-if __name__ == '__main__':
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-    if len(argv) != 4:
-        print("error")
-    else:
-        USER = argv[1]
-        PASS = argv[2]
-        DB = argv[3]
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(USER, PASS, DB), pool_pre_ping=True)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        add_state = State(name='Louisiana')
-        session.add(add_state)
-        session.commit()
-        print(add_state.id)
+if __name__ == "__main__":
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    add_state = State(name="Louisiana")
+    session.add(add_state)
+    # commit and close session
+    session.commit()
+    print(add_state.id)
+    session.close()
