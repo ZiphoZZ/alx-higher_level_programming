c6be161c596da8ff98632b5ba80b099efb8da576
ZiphoZZ <113602509+ZiphoZZ@users.noreply.github.com>
2024-03-21 20:32:18 +0200

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,24 +1,27 @@
-
 #!/usr/bin/python3
-"""A script that lists all states"""
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
 
-if __name__ == '__main__':
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-    if len(argv) != 4:
-        print("error")
-    else:
-        USER = argv[1]
-        PASS = argv[2]
-        DB = argv[3]
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(USER, PASS, DB), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    for states in session.query(State):
-        print("{}: {}".format(states.id, states.name))
+
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
