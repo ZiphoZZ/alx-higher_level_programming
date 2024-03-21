4b4830c428b59e3d38bb342873b6c84751046e36
ZiphoZZ <113602509+ZiphoZZ@users.noreply.github.com>
2024-03-21 19:15:41 +0200

    Update 12-model_state_update_id_2.py
    changes

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,23 +1,25 @@
-
 #!/usr/bin/python3
-"""A script that lists all states"""
+"""
+Script that changes the name of a State object to the database
+Using module SQLAlchemy
+"""
 
-if __name__ == '__main__':
-    from sys import argv
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
-    if len(argv) != 4:
-        print("error")
-    else:
-        USER = argv[1]
-        PASS = argv[2]
-        DB = argv[3]
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                               .format(USER, PASS, DB), pool_pre_ping=True)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        update_state = session.query(State).filter(State.id == 2).first()
-        update_state.name = 'New Mexico'
-        session.commit()
+if __name__ == "__main__":
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # create a Session
+    session = Session()
+    Base.metadata.create_all(engine)
+    state_update = session.query(State).filter_by(id='2').first()
+    state_update.name = "New Mexico"
+    # commit and close session
+    session.commit()
+    session.close()
